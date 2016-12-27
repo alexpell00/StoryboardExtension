4617e70a554d908d07a9625d1a6d2b0e26201394
Alex Pelletier <accounts@alexp.io>
2016-12-27 05:40:30 -0700

    Added uiview extension

---
+++ UIView.swift
@@ -0,0 +1,93 @@
+//
+//  UIView.swift
+//  
+//
+//  Copyright © 2016 Alex Pelletier. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return layer.cornerRadius
+        }
+        set {
+            layer.cornerRadius = newValue
+            layer.masksToBounds = newValue > 0
+        }
+    }
+    @IBInspectable var maskToBounds: Bool {
+        get {
+            return layer.masksToBounds
+        }
+        set {
+            layer.masksToBounds = newValue
+        }
+    }
+    @IBInspectable var borderColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.borderColor!)
+        }
+        set {
+            layer.borderColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat {
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth = newValue
+        }
+    }
+    
+    @IBInspectable var shadowColor: UIColor {
+        get {
+            return UIColor(cgColor: layer.shadowColor!)
+        }
+        set {
+            layer.shadowColor = newValue.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable var rotation: Double {
+        get {
+            let radians = atan2f(Float(transform.b), Float(transform.a));
+            let degrees = Double(radians) * (180 / M_PI);
+            return (degrees)
+        }
+        set {
+            self.transform = CGAffineTransform(rotationAngle: CGFloat(newValue * M_PI / 180))
+        }
+    }
+}
